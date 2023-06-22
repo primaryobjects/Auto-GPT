f1f517a9bab437efb758613f9e64eb4aa16f3f5d
Kory Becker <kbecker@primaryobjects.com>
2023-06-22 12:13:56 -0400

    Python executable.

--- autogpt/commands/execute_code.py
+++ autogpt/commands/execute_code.py
@@ -292,4 +292,5 @@ def we_are_running_in_a_docker_container() -> bool:
     Returns:
         bool: True if we are running in a Docker container, False otherwise
     """
-    return os.path.exists("/.dockerenv")
+    return True
+    #return os.path.exists("/.dockerenv")
